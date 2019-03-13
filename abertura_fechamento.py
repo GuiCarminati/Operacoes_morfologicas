74c72950ed3a8c473332c6177ab8430e9a732799
Anderson <anderon_mm@outlook.com.br>
2019-03-13 14:23:30 -0300

    Melhora plot

--- abertura_fechamento.py
+++ abertura_fechamento.py
@@ -4,19 +4,23 @@ import matplotlib.pyplot as plt
 
 # abre imgem e mostra
 imagem1 = cv2.imread('imagens/img2.png', 0)
+plt.figure('Original 1')
 plt.imshow(imagem1, cmap='gray')
-plt.show()
+
 imagem2 = cv2.imread('imagens/img3.png', 0)
+plt.figure('Original 2')
 plt.imshow(imagem2, cmap='gray')
-plt.show()
 
 # cria elemento estruturante
 elem_e = np.ones((3, 3), np.uint8)
 
 # aplica a operacao e mostra resultado
 abertura = cv2.morphologyEx(imagem1, cv2.MORPH_OPEN, elem_e)
+plt.figure('Abertura')
 plt.imshow(abertura, cmap='gray')
-plt.show()
+
 fechamento = cv2.morphologyEx(imagem2, cv2.MORPH_CLOSE, elem_e)
+plt.figure('Fechamento')
 plt.imshow(fechamento, cmap='gray')
+
 plt.show()
--- dualidade.py
+++ dualidade.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 
 # abre imgem e mostra
 imagem = cv2.imread('imagens/img1.png', 0)
+plt.figure('Original')
 plt.imshow(imagem, cmap='gray')
-plt.show()
 
 # inverte imagem
 imagem = cv2.bitwise_not(imagem)
@@ -16,9 +16,12 @@ elem_e = np.ones((3, 3), np.uint8)
 # aplica a operacao e mostra resultado
 erosao = cv2.erode(imagem, elem_e, iterations=1)
 erosao = cv2.bitwise_not(erosao)
+plt.figure('Erosao com o complemento')
 plt.imshow(erosao, cmap='gray')
-plt.show()
+
 dilatacao = cv2.dilate(imagem, elem_e, iterations=1)
 dilatacao = cv2.bitwise_not(dilatacao)
+plt.figure('Dilatacao com o complemento')
 plt.imshow(dilatacao, cmap='gray')
+
 plt.show()
